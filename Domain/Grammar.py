515a95470a307375f34a22872d46a19cc48b297a
Mozes Csaszar <mozes.csaszar@stud.ubbcluj.ro>
2023-12-07 23:56:13 +0200

    Add terminal and non-terminal support + automatic grammar enchantment

--- Domain/Grammar.py
+++ Domain/Grammar.py
@@ -13,20 +13,28 @@ class Grammar:
     def _create(self, file_name):
         # 1) read from file, get productions and symbols
         with open("InputFiles/" + file_name, "r") as f:
+            self.non_terminals = set(f.readline().strip().split(" "))
+            self.terminals = set(f.readline().strip().split(" "))
+            # the symbols can be gotten by   terminals U non_terminals
+            self.symbols = self.terminals.union(self.non_terminals)
             # get and number all the productions
             for line in f.readlines():
-
                 lhs, rhss = line.split("->")
                 lhs = lhs.strip()
-                # add left hand side to non-terminals and symbols
-                self.symbols.add(lhs)
-                self.non_terminals.add(lhs)
-                rhss = rhss.split("|")
-                for rhs in rhss:
-                    rhs = tuple(filter(lambda s: s != "", map(lambda s: s.strip(), rhs.split(" "))))
-                    self.productions.append(Production(len(self.productions), lhs, rhs))
-                    # add all symbols from right hand side to symbols
-                    for s in rhs:
-                        self.symbols.add(s)
-        # the terminals can be gotten by symbols \ non_terminals
-        self.terminals = self.symbols.difference(self.non_terminals)
+                # check if LHS is a non-terminal
+                if lhs in self.non_terminals:
+                    rhss = rhss.split("|")
+                    for rhs in rhss:
+                        rhs = tuple(filter(lambda s: s != "", map(lambda s: s.strip(), rhs.split(" "))))
+                        # check each element to be part of the alphabet
+                        for elem in rhs:
+                            if elem not in self.symbols:
+                                raise Exception("ERROR: Component of RHS `{}` is not a symbol from the alphabet!".format(elem))
+                        self.productions.append(Production(len(self.productions), lhs, rhs))
+                else:
+                    raise Exception("ERROR: LHS `{}` is not a non-terminal from the alphabet!".format(lhs))
+        lhs = "S'"
+        while lhs in self.non_terminals:
+            lhs += "'"
+        self.productions.insert(0, Production(-1, lhs, [self.productions[0].lhs]))
+        self.non_terminals.add(lhs)
